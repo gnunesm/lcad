d48a4bf783e1949f46c95fb8ba8735f77913ae64
gnunesm <gabnunes2112@gmail.com>
2019-09-16 14:07:08 -0300

    new stuff

--- scripts/get_equivalent_velodynes.py
+++ scripts/get_equivalent_velodynes.py
@@ -8,7 +8,8 @@ def find_nearest(array, value):
     return idx
 
 src_bumblebee = '/dados/calib_bumblebee/'
-src_velodyne = '/dados/calib_velodyne/'
+# src_velodyne = '/dados/calib_velodyne/'
+src_velodyne = '/dados/vel_pngs/'
 dest_bumblebee = '/dados/bb3/'
 dest_velodyne = '/dados/velodyne/'
 
@@ -19,8 +20,9 @@ img_times = [float(a.rsplit('.', 1)[0].split('-')[0]) for a in img_list]
 pointcloud_times = [float(a.rsplit('.', 1)[0]) for a in pointcloud_list]
 
 for idx, img_name in enumerate(img_list):
-    copyfile(src_bumblebee+img_name, dest_bumblebee+'img'+'{0:04d}'.format(idx+1)+'.png')
     time = img_times[idx]
     pointcloud_idx = find_nearest(pointcloud_times, time)
-    copyfile(src_velodyne+pointcloud_list[pointcloud_idx], dest_velodyne+'cloud'+'{0:04d}'.format(idx+1)+'.txt')
-    print(img_name, pointcloud_list[pointcloud_idx])
+    if pointcloud_times[pointcloud_idx] <= 1563468272.5:
+        copyfile(src_bumblebee+img_name, dest_bumblebee+'img'+'{0:04d}'.format(idx+1)+'.png')
+        copyfile(src_velodyne+pointcloud_list[pointcloud_idx], dest_velodyne+'cloud'+'{0:04d}'.format(idx+1)+'.png')
+        print(img_name, pointcloud_list[pointcloud_idx])
---
+++ scripts/pointcloud_txt_to_png.py
@@ -0,0 +1,8 @@
+import os
+import cv2
+import numpy as np
+
+src_bumblebee = '/dados/bb3/'
+src_velodyne = '/dados/velodyne/'
+dest_bumblebee = '/dados/cropped_bb3/'
+dest_velodyne = '/dados/velodyne/''
